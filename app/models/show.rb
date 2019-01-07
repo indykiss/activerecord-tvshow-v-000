56a10e6dfdef19e05f1c24d697dd47d2bc7ac960
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-07 05:08:18 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,7 +7,7 @@ class Show < ActiveRecord::Base
   end 
 
   def self.most_popular_show 
-    self.where("rating = ?", highest_rating)[0]
+    Show.find_by("rating = ?", highest_rating)
   end 
   
   
