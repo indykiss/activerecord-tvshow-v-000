d9ad12849639d6860b5fa4dc7cd164cb85a0eba5
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-07 05:09:15 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -14,7 +14,7 @@ class Show < ActiveRecord::Base
     Show.minimum(:rating)
   end 
   
-  def self.most_popular_show 
+  def self.least_popular_show 
     Show.find_by("rating = ?", highest_rating)
   end 
 
