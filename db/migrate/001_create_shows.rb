219cdba3f060568f3bd92f7ca30f053f7d85d2de
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-07 04:49:49 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,13 +1,10 @@
-
-class CreateShows < ActiveRecord::Migration[5.2]
-  
-  def change 
+class CreateShows < ActiveRecord::Migration[5.1]
+  def change
     create_table :shows do |t|
-      t.string :name 
+      t.string :name
       t.string :network
-      t.string :day 
-      t.integer :rating 
-    end 
-  end 
-  
-end 
+      t.string :day
+      t.integer :rating
+    end
+  end
+end
